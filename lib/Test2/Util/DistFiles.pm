e18b9e95db40317f9a4d193d3bda8aa333f176e8
Robert Rothenberg <rrwo@cpan.org>
2025-07-23 09:10:07 +0100

    Rename BUGS to SUPPORT

--- lib/Test2/Util/DistFiles.pm
+++ lib/Test2/Util/DistFiles.pm
@@ -141,12 +141,19 @@ sub is_perl_file {
 
 L<Test::XTFiles>
 
-=head1 append:BUGS
+=head1 prepend:SUPPORT
 
-=head2 Reporting Security Vulnerabilities
+Only the latest version of this module will be supported.
 
-Security issues should not be reported on the bugtracker website. Please see F<SECURITY.md> for instructions how to
-report security vulnerabilities
+This module requires Perl v5.14 or later.  Future releases may only support Perl versions released in the last ten
+years.
+
+=head2 Reporting Bugs
+
+=head1 append:SUPPORT
+
+If the bug you are reporting has security implications which make it inappropriate to send to a public issue tracker,
+then see F<SECURITY.md> for instructions how to report security vulnerabilities.
 
 =cut
 
